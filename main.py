cfea11a94f5b56e2f72247fb33371f0247d762cd
GeekchanskiY <astrovskiy.04@gmail.com>
2022-06-25 14:50:20 +0300

    comments added

--- main.py
+++ main.py
@@ -4,35 +4,48 @@ from phonenumbers.phonenumberutil import NumberParseException
 from openpyxl import Workbook, load_workbook, worksheet
 
 
-def select_number_from_str(string: str) -> str:
+def alternative_select_number_from_str(string: str) -> str:
+
+    # deleting all unnecessary symbols
 
     string = re.sub("[а-я]", "", string)
     string = re.sub("[А-Я]", "", string)
     string = re.sub("[a-z]", "", string)
     string = re.sub("[A-Z]", "", string)
+
+    # deleting spaces
     string = string.strip()
+
+    # for (xxx) case
     if string[0] == "(" and string[-1] == ")":
         string = string[1:-1]
 
-    phone_len = 0
+    # flag for getting code
     code_starts = False
     code = ""
     this_phone = ""
     for i in range(0, len(string)):
+
+        # getting code
         if string[i] == "(":
             code_starts = True
         elif string[i] == ")":
             code_starts = False
+
+        # if numeric value in string pos, writing this number to code or phone
         if re.match(r"\d", string[i]):
             if code_starts is True:
                 code += string[i]
             else:
                 this_phone += string[i]
+
+    # concat code with phone
     this_phone = code + this_phone
+
     return this_phone
 
 
-def filetype1(wb: Workbook, *args, **kwargs) -> Workbook:
+def filetype1(wb: Workbook) -> Workbook:
     """
         Makes new worksheet for filetype 1 and returns 1 if created successfully
 
@@ -40,7 +53,7 @@ def filetype1(wb: Workbook, *args, **kwargs) -> Workbook:
                wb (Workbook): original workbook
 
         Returns:
-            Workbook: new workbook with formatted data
+            Workbook: new Workbook with formatted data
     """
 
     output_data: list[str, list] = []
@@ -51,7 +64,7 @@ def filetype1(wb: Workbook, *args, **kwargs) -> Workbook:
 
         # First filter with general regex
 
-        for m in re.finditer(r"(?:(?:8|\+7)[\- ]?)?(?:\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}", str(row[1])):
+        for m in re.finditer(r"(?:(?:8|\+7)[\- ]?)?(?:\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}", str(row[2])):
 
             # Second filter with phonenumbers
 
@@ -71,8 +84,13 @@ def filetype1(wb: Workbook, *args, **kwargs) -> Workbook:
             if len(str(re.sub(r"\D", "", temp_str))) >= 7:
                 for t_s in temp_str.split(","):
                     if t_s != "" and len(re.sub("\D", "", t_s)) >= 7:
-                        numbers.append(select_number_from_str(t_s))
+                        numbers.append(alternative_select_number_from_str(t_s))
 
+        # deleting duplicates if exists
+
+        numbers = list(set(numbers))
+
+        print(numbers)
     print(output_data)
     return wb
 
@@ -103,5 +121,3 @@ def main(filename: str, *args, **kwargs):
 
 if __name__ == '__main__':
     main("test1.xlsx")
-
-
